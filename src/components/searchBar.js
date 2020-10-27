629eff19098e6278aa91245460c2cfcfb8ac2c59
rishabh verma <vermarishabh0987@gmail.com>
2020-10-27 19:09:56 +0530

    made space for search icon

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,7 +3,9 @@ import "./searchBar.css";
 function SearchBar() {
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Type here.." value=""></input>
+      <div className="search">
+        <input type="text" placeholder="Type here.." value=""></input>
+      </div>
       <div className="des">
         <p>use Enter to add items</p>
         <p>Use UP and DOWN key to navigate between suggestions</p>
